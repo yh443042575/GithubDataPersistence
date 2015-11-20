784e0fb196e6ecccefb540a9b5db6022faee7946
yh443042575 <443042575@qq.com>
2015-11-20 23:23:57 +0800

    列出解决思路，方便日后实现

--- src/edu/hit/yh/mine/algorithm/AbstractGspMiningAlgorithm.java
+++ src/edu/hit/yh/mine/algorithm/AbstractGspMiningAlgorithm.java
@@ -15,24 +15,49 @@ import lombok.Setter;
 public abstract class AbstractGspMiningAlgorithm<P> {
 
 	//算法的支持度
-	private int surpport;
+	private int minSurpport;
+	
+	//算法所要挖掘的项目
+	private String programName;
+	
+	//算法所要挖掘的artifactType
+	private String artifactType;
 	
 	// 算法的总执行
 	public abstract void execute(Object... args);
 
 	//数据库的扫描
-	public abstract List<P> scanDB(List<P> patternlist);
+	public abstract List<P> scanForPattern(List<P> patternlist);
 	
 	//连接操作
 	public abstract List<P> joinOperation(List<P> patternlist);
 
-	public int getSurpport() {
-		return surpport;
+	public int getMinSurpport() {
+		return minSurpport;
 	}
 
-	public void setSurpport(int surpport) {
-		this.surpport = surpport;
+	public void setMinSurpport(int minSurpport) {
+		this.minSurpport = minSurpport;
 	}
+
+	public String getArtifactType() {
+		return artifactType;
+	}
+
+	public void setArtifactType(String artifactType) {
+		this.artifactType = artifactType;
+	}
+
+	public String getProgramName() {
+		return programName;
+	}
+
+	public void setProgramName(String programName) {
+		this.programName = programName;
+	}
+
+	
+	
 	
 	
 	
--- src/edu/hit/yh/mine/algorithm/SimpleGspMiningAlgorithm.java
+++ src/edu/hit/yh/mine/algorithm/SimpleGspMiningAlgorithm.java
@@ -1,26 +1,59 @@
 package edu.hit.yh.mine.algorithm;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.hit.yh.mine.module.BehaviorPattern;
+import edu.hit.yh.mine.module.SimpleBehavior;
 
-public class SimpleGspMiningAlgorithm extends AbstractGspMiningAlgorithm<BehaviorPattern> {
+/**
+ * 挖掘最简单的
+ * @author emmerich.yh
+ *
+ */
+public class SimpleGspMiningAlgorithm extends AbstractGspMiningAlgorithm<BehaviorPattern<SimpleBehavior>> {
 
+	/**
+	 * 算法中的行为类型：
+	 * 1、实际的开发者、简单类型——SimpleBehavior
+	 * 2、实际的开发者、带有相对时间类型——RelativeTimeBehavior
+	 * 3、抽象的开发者、简单类型
+	 * 4、抽象的开发者、带有相对时间类型
+	 * 
+	 */
 	
-	public SimpleGspMiningAlgorithm(int surpport){
-		super.setSurpport(surpport);
+	
+	/**
+	 * 构造函数，用来规定算法的最小支持度
+	 * @param behaviorType
+	 * @param surpport
+	 */
+	public SimpleGspMiningAlgorithm(int minSurpport,String programName,String artifactType){
+		
+		super.setMinSurpport(minSurpport);
+		super.setProgramName(programName);
+		super.setArtifactType(artifactType);
+	
 	}
 	
 	@Override
 	public void execute(Object... args) {
 		// TODO 简单的GSP执行，扫描数据库->筛选出长度为K的序列模式->连接长度为K的序列模式生成K+1的候选序列模式->再次扫描数据库->...
+		List<List<BehaviorPattern<SimpleBehavior>>> rawData = this.getRawData();
+		int nowSupport = Integer.MAX_VALUE;
+
+		//当patternList中的所有pattern的支持度都大于这个最小支持度时，则继续扫描
+		while(nowSupport>getMinSurpport()){
+			rawData.get(0);
+		}
+		//遍历每一个artifactList,找到
 		
 	}
 
 	
 	@Override
-	public List<BehaviorPattern> scanDB(List<BehaviorPattern> patternlist) {
-		// TODO 穿入长度为K的候选序列模式，筛选出支持度满足支持度大于等于K的模式，作为序列模式
+	public List<BehaviorPattern<SimpleBehavior>> scanForPattern(List<BehaviorPattern<SimpleBehavior>> patternlist) {
+		// TODO 传入长度为K的候选序列模式，筛选出支持度满足支持度大于等于K的模式，作为序列模式
 		if(patternlist.size()==0){//如果初始时pattern个数为0，则扫描数据库，只要大于支持度的项全取出来
 			
 		}else{//同样扫描数据库，通过时间可以位移的方法来判断我们序列模式的计数
@@ -31,11 +64,33 @@ public class SimpleGspMiningAlgorithm extends AbstractGspMiningAlgorithm<Behavio
 	}
 
 	@Override
-	public List<BehaviorPattern> joinOperation(List<BehaviorPattern> patternlist) {
+	public List<BehaviorPattern<SimpleBehavior>> joinOperation(List<BehaviorPattern<SimpleBehavior>> patternlist) {
 		// TODO 对现有的pattern做连接操作
 		return null;
 	}
 
-	
+	/**
+	 * 准备原始数据，最里层的list是一个artifact，其中由先后顺序排列好
+	 * 1、连接数据库，查询所有在同一个program下的、同种artifactType的操作
+	 * 2、将这些操作通过给定的artifactID分类并挑选出来，同一个artifactId下的操作都放在一个list中
+	 * @param programName
+	 * @param artifactType
+	 * @return
+	 */
+	private List<List<BehaviorPattern<SimpleBehavior>>> getRawData(){
+		if(getProgramName().isEmpty()||getArtifactType().isEmpty()){
+			System.out.println("programName或者artifactType为空，请检查参数");
+			return null;
+		}
+		
+		List<List<BehaviorPattern<SimpleBehavior>>> rawData = new ArrayList<List<BehaviorPattern<SimpleBehavior>>>();
+		
+		//获取session
+		
+		//查询数据库
+		
+		//转换结果
+		return rawData;
+	}
 
 }
